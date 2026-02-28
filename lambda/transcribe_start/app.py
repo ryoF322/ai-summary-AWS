21c8dd937e31963bdbf7b017c98f832c7e949251
ryo322 <syaro12@icloud.com>
2026-02-28 16:13:57 +0900

    unpdate app.py

--- lambda/transcribe_start/app.py
+++ lambda/transcribe_start/app.py
@@ -66,6 +66,7 @@ def lambda_handler(event, context):
                 'MaxSpeakerLabels': 2,        # Amazon Connectは基本2者通話
                 'ShowAlternatives': False,
             },
+            OutputEncryptionKMSKeyId=os.environ.get('KMS_KEY_ID')
         )
 
         job_info = response['TranscriptionJob']
